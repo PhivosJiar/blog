25ef9c19ac2a77fcacbf9618de428852c33ad1ff
PhivosJiar <paulyuancho@gmail.com>
2021-10-05 10:38:58 +0800

    UI新增錯誤提示

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router }          from '@angular/router';
+import { Router } from '@angular/router';
 
 const Parse = require('parse');
 @Component({
@@ -8,9 +8,9 @@ const Parse = require('parse');
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  account:string='';
-  password:string='';
+  errCode: string = '';
+  account: string = '';
+  password: string = '';
   constructor(
     private route: Router,
   ) { }
@@ -20,19 +20,25 @@ export class LoginComponent implements OnInit {
     if (currentUser) {
       this.route.navigate(['/home']);
     } else {
-  
+
     }
   }
 
-  async login(){
-    if(this.account.trim()!='' && this.password.trim()!=''){
-    await Parse.User.logIn(this.account, this.password).then(() => {
-      console.log("success");
-      this.route.navigate(['/home']);
-    }, function (error:any) {
-      console.log("false");
-    });
-   }
+  async login() {
+    this.errCode = ''
+    if (this.account.trim() != '' && this.password.trim() != '') {
+      await Parse.User.logIn(this.account, this.password).then(() => {
+        console.log("success");
+        this.route.navigate(['/home']);
+      }, (error: any) => {
+        console.log(error)
+        if (error = 'Invalid username/password.') {
+          this.errCode = '帳號或密碼錯誤';
+        }
+      });
+    } else {
+      this.errCode = '帳號或密碼為空'
+    }
   }
 
 }
--- src/app/login/signup/signup.component.ts
+++ src/app/login/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router }          from '@angular/router';
+import { Router } from '@angular/router';
 
 const Parse = require('parse');
 @Component({
@@ -8,23 +8,32 @@ const Parse = require('parse');
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  
-  account:string='';
-  password:string='';
-  constructor( 
+  errCode: string = '';
+  account: string = '';
+  password: string = '';
+  constructor(
     private route: Router,
-    ) { }
+  ) { }
 
   ngOnInit(): void {
-    
-  }
-  signup(){
-    let user = new Parse.User();
-    user.set("username", this.account);
-    user.set("password", this.password);
-    user.signUp().then(()=>{
-      this.route.navigate(['/']);
-    });
 
+  }
+  signup() {
+    this.errCode=''
+    if (this.account.trim() != '' && this.password.trim() != '') {
+      let user = new Parse.User();
+      user.set("username", this.account);
+      user.set("password", this.password);
+      user.signUp().then(() => {
+        this.route.navigate(['/']);
+      }, (error: any) => {
+        console.log(error)
+        if (error = 'Account already exists for this username.') {
+          this.errCode = '帳號已存在';
+        }
+      });
+    }else{
+      this.errCode='帳號或密碼為空'
+    }
   }
 }
